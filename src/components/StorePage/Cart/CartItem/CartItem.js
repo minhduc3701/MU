9f971c3b04360a265ff4c9a736716480dbd11746
minhduc <minh.duc.3701@gmail.com>
2019-10-28 18:23:41 +0700

    update cartitem design

--- src/components/StorePage/Cart/CartItem/CartItem.js
+++ src/components/StorePage/Cart/CartItem/CartItem.js
@@ -9,9 +9,25 @@ class CartItem extends React.Component {
       <div className="cart__item">
         <div className="cart__info">
           <img src={img1} alt="" />
-          <p>M | Front print and paisley print Quantity: 1</p>
+          <div>
+            M | Front print and paisley print
+            <p>Quantity: 1</p>
+          </div>
         </div>
-        <p className="cart__price">102$</p>
+        <div className="cart__action">
+          <p className="cart__price">$ 102</p>
+          <div className="btn__group">
+            <button className="btn__change" type="button" disabled>
+              -
+            </button>
+            <button className="btn__change" type="button">
+              +
+            </button>
+          </div>
+        </div>
+        <button type="button" className="close">
+          X
+        </button>
       </div>
     );
   }
--- src/components/StorePage/Product/ProductItem/ProductItem.js
+++ src/components/StorePage/Product/ProductItem/ProductItem.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import "./ProductItem.scss";
 import img1 from "./51Es1j6oobL._AC_SX425_.jpg";
@@ -6,14 +6,14 @@ import img1 from "./51Es1j6oobL._AC_SX425_.jpg";
 class ProductItem extends React.Component {
   render() {
     return (
-      <Fragment>
+      <div className="product__card">
         <div className="product__item">
           <img src={img1} />
           <p>main KIT 2019/20</p>
           <span className="cart__price">102$</span>
           <button className="product__btn">Add to cart</button>
         </div>
-      </Fragment>
+      </div>
     );
   }
 }
